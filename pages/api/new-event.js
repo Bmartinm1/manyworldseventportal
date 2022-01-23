06b2670b3eccce086b5388cd72a88d90c3b3721c
Ben MartinMooney <ben.martinmooney@gmail.com>
2022-01-23 16:22:20 -0500

    finished basic functionality

---
+++ pages/api/new-event.js
@@ -0,0 +1,25 @@
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
+	if (req.method === 'POST') {
+		const data = req.body;
+
+		const { title, image, description, id } = data;
+
+		const client = await MongoClient.connect(
+			'mongodb+srv://ben_m_squared:Thetrinity1@cluster0.fwv4v.mongodb.net/eventsDatabase?retryWrites=true&w=majority'
+		);
+		const db = client.db();
+
+		const eventsCollection = db.collection('events');
+
+		const result = await eventsCollection.insertOne(data);
+		console.log(result);
+
+		client.close();
+
+		res.status(201).json({ message: 'Event successfully added!' });
+	}
+}
+
+export default handler;
